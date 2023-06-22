744fa8b0386658c848754e7257e6340581731851
송현서 <s22026@gsm.hs.kr>
2023-06-22 12:15:30 +0900

    InputActivity Ui

---
+++ app/src/main/java/com/todo/presentation/InputActivity.kt
@@ -0,0 +1,23 @@
+package com.todo.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.todo.databinding.ActivityInputBinding
+
+class InputActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInputBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInputBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+}
